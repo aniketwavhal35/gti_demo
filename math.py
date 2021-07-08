10750a0ec120cef62013b977994df85f42edd933
aniketwavhal35 <aniketwavhal35@gmail.com>
2021-07-08 14:36:49 +0530

    Added fn header for all functions

--- math.py
+++ math.py
@@ -1,10 +1,14 @@
+#add implemetation
 #asdas
 def add(x,y):
     pass
     #subtract header
+#ubtract implementaiom
 def subtract(x,y):
     pass
+#umu implementaiom
 def multiply(x,y):
     pass
+#divide implementaiom
 def divide(x,y):
     pass
